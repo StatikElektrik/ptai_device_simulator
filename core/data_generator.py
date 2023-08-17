ac40521ce98280801e6bb0ed7c525f870fe33656
Gökhan Koçmarlı <gokhan.kocmarli@gmail.com>
2023-08-17 23:40:06 +0300

    refactor: prepare new interface

--- core/data_generator.py
+++ core/data_generator.py
@@ -8,59 +8,27 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class RandomGenerator:
-    """This class is responsible for generating random list for the analysis."""
-
-    def __init__(self, start_point: float):
-        self.start_point = start_point
-
-    def generate_random_array(self, array_size: int):
-        """It generates a random array with the given size.
-
-        Parameters
-        ----------
-        array_size : int
-            The size of the array to be generated. It must be in the range [1, 10].
-
-        Returns
-        -------
-        list
-            A list with the random values.
-        """
-        random_array = [
-            random.randint(self.start_point, self.start_point + 20)
-            for _ in range(array_size)
-        ]
-        self.start_point += random.randint(1, 10)
-        return random_array
-
-
 class DataGenerator:
     """This class is responsible for generating data format for the analysis
     just like recieved from MCU.
     """
 
-    @staticmethod
-    def generate_analysis_data(random_generators) -> dict:
-        """It generates a dictionary with the analysis data.
+    def __init__(
+        self,
+        buffer_size: int,
+        function_type: str,
+        function_parameters: dict[str, float],
+    ) -> None:
+        pass
 
-        Parameters
-        ----------
-        random_generators : @TODO What is this?
-            @TODO What is this?
+    def generate(self) -> list:
+        """@TODO: What is this?"""
 
-        Returns
-        -------
-        dict
-            A dictionary with the analysis data that can be thought
-            as its recieved from MCU AI component.
-        """
-        data_points_number = random.randint(1, 10)
-        return {
-            "np": data_points_number,
-            "e1": random_generators[0].generate_random_array(data_points_number),
-            "e2": random_generators[1].generate_random_array(data_points_number),
-            "e3": random_generators[2].generate_random_array(data_points_number),
-            "e4": random_generators[3].generate_random_array(data_points_number),
-            "e5": random_generators[4].generate_random_array(data_points_number),
-        }
+    def _calculate_max_element(self, frequency: int, duration: int) -> int:
+        """@TODO: What is this?"""
+
+    def _add_error_to_data(self, data: list, error_percentage: int) -> list:
+        """@TODO: What is this?"""
+
+    def _calculate_function_value(self, time_val: int) -> float:
+        """@TODO: What is this?"""
