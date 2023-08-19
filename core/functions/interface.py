6aa009cdcf02592f71f7c692a28a9f2e2288e20e
Merve Varlı <mervve_varli@hotmail.com>
2023-08-19 18:53:26 +0300

    feat: add common function for plotting

--- core/functions/interface.py
+++ core/functions/interface.py
@@ -4,24 +4,45 @@ implementations. It defines the interface that all
 subclasses must implement.
 """
 
-from abc import ABC, abstractmethod, abstractstaticmethod
+from abc import ABC, abstractmethod
+from matplotlib import pyplot as plt
 
 class BaseFunction(ABC):
     """Follow this interface to implement a function for
     DataGenerator.
     """
 
-    def __init__(self) -> None:
-        super().__init__()
+    @abstractmethod
+    def calculate(self, x_value: float) -> float:
+        """The interface function to calculate the function."""
 
     @abstractmethod
-    def calculate(self, x: float) -> float:
-        pass
+    def inverse(self, y_value: float) -> float:
+        """The interface function to calculate the inverse of the function."""
 
-    @abstractmethod
-    def inverse(self, y: float) -> float:
-        pass
+    @staticmethod
+    def plot(x_values: list, y_values: list, plot_type="linear") -> None:
+        """This function is used to plot the function. Do not
+        implement this function in the subclass. This function
+        is implemented in the base class to provide a common
+        interface for all the subclasses.
 
-    @abstractstaticmethod
-    def plot() -> None:
-        pass
+        Parameters
+        ----------
+        x_values : list
+            The inputs to the function.
+        y_values : list
+            The outputs of the function.
+        plot_type : str, optional
+            "linear", "exponantial", "dbscale", by default "linear"
+        """
+
+        if plot_type == "linear":
+            # Plot the function in linear scale.
+            plt.plot(x_values, y_values)
+            plt.x_label("x_values")
+            plt.y_label("y_values")
+            plt.title("Linear Scale Plot of The Function")
+            plt.show()
+        else:
+            raise NotImplementedError
